55a05c421c82b68f56021421cda4b098f143d4de
Yoslandy <yoslandy.lopez@guadaltech.es>
2022-03-23 17:22:15 +0100

    [ADD] gst_tsosl module

---
+++ tsosl/gst_tsosl/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'GST TSOSL',
+    'version': '1.0',
+    'category': 'CRM',
+    'depends': ['crm'],
+    'data': [
+  
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': True,
+    'license': 'LGPL-3',
+}
---
+++ tsosl/gst_tsosl/models/__init__.py
@@ -0,0 +1,3 @@
+from . import crm_lead
+
+
